4eb4b1d05ea52d1420e3ee60d1c8195cdc67e3d0
sofylopdev <sofylopdev@gmail.com>
2018-08-04 14:57:43 +0100

    Adding Projects

---
+++ AdvancedAndroid_MyGarden/app/src/main/java/com/example/android/mygarden/GridWidgetService.java
@@ -0,0 +1,118 @@
+package com.example.android.mygarden;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.ui.PlantDetailActivity;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+/**
+ * Created by Sofia on 4/16/2018.
+ */
+public class GridWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new GridRemoteViewsFactory(this.getApplicationContext());
+    }
+}
+
+class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    Context mContext;
+    Cursor mCursor;
+
+    public GridRemoteViewsFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+
+        if (mCursor != null) mCursor.close();
+
+        mCursor = mContext.getContentResolver().query(
+                PLANT_URI, null, null, null,
+                PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        mCursor.close();
+    }
+
+    @Override
+    public int getCount() {
+        if (mCursor == null) return 0;
+        return mCursor.getCount();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        if (mCursor == null || mCursor.getCount() == 0) return null;
+        mCursor.moveToPosition(position);
+
+        int idIndex = mCursor.getColumnIndex(PlantContract.PlantEntry._ID);
+        int createTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+        int waterTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+        int plantTypeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+
+        long plantId = mCursor.getLong(idIndex);
+        int plantType = mCursor.getInt(plantTypeIndex);
+        long createdAt = mCursor.getLong(createTimeIndex);
+        long wateredAt = mCursor.getLong(waterTimeIndex);
+        long timeNow = System.currentTimeMillis();
+
+        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.plant_widget);
+
+        int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow - createdAt, timeNow - wateredAt, plantType);
+
+        views.setImageViewResource(R.id.widget_plant_image, imgRes);
+        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+        views.setViewVisibility(R.id.widget_water_button, View.GONE);
+
+        Bundle extras = new Bundle();
+        extras.putLong(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtras(extras);
+        views.setOnClickFillInIntent(R.id.widget_plant_image, fillInIntent);
+
+        return views;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
